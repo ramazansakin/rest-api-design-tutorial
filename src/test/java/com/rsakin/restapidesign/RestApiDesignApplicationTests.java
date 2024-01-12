aeb4edff3f1a4c3df85b7a65cf35fbc2113e09eb
ramazansakin <ramazansakin63@gmail.com>
2024-01-12 15:32:36 +0300

    feat: initializing a basic note app with CRUD ops

---
+++ src/test/java/com/rsakin/restapidesign/RestApiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsakin.restapidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
